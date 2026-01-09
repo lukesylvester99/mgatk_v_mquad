dc6fe4fd5e0a62b9561bd2747d882c01727a5449
Luke Sylvester <145699572+lukesylvester99@users.noreply.github.com>
2026-01-09 15:03:59 -0800

    added envs section and conda env to rules

--- snakefile.py
+++ snakefile.py
@@ -3,41 +3,44 @@ import os
 # --------- Load Config Variables ---------
 configfile: "config.yaml"
 
-#global variables
+# global variables
 SAMPLE = config["sample"]
 BASE_DIR = os.path.expanduser(os.path.expandvars(config["base_dir"]))
 OUT_DIR = config["out_dir"].format(base_dir=BASE_DIR)
 OUT_DIR = os.path.expanduser(os.path.expandvars(OUT_DIR))
 
-#epianeufinder pipeline paths
+# epianeufinder pipeline paths
 BLACKLIST = os.path.expanduser(os.path.expandvars(config["blacklist"]))
 CLEANED_FRAGS = os.path.expanduser(os.path.expandvars(config["cleaned_frags"]))
 
 
-
 # --------- Rules ---------
 rule all:
-    input: 
+    input:
         os.path.join(
-            OUT_DIR, SAMPLE, "epi_results", "epiAneufinder_results", f"{SAMPLE}_cells_for_mquad.tsv"
+            OUT_DIR, SAMPLE, "epi_results", "epiAneufinder_results",
+            f"{SAMPLE}_cells_for_mquad.tsv"
         )
+
 
 rule run_epi:
     """Run epiAneufinder and require all expected output files."""
     input:
         frags=CLEANED_FRAGS,
         blacklist=BLACKLIST,
-        r_script=os.path.join(BASE_DIR, "workflows", "scripts", "run_epi.R")
+        r_script="../scripts/run_epi.R"
     params:
         outdir=os.path.join(OUT_DIR, SAMPLE, "epi_results")
     output:
-        cnv_calls           = os.path.join(OUT_DIR, SAMPLE, "epi_results", "epiAneufinder_results", "cnv_calls.rds"),
-        counts_gc_corrected = os.path.join(OUT_DIR, SAMPLE, "epi_results", "epiAneufinder_results", "counts_gc_corrected.rds"),
-        count_summary       = os.path.join(OUT_DIR, SAMPLE, "epi_results", "epiAneufinder_results", "count_summary.rds"),
-        karyogram           = os.path.join(OUT_DIR, SAMPLE, "epi_results", "epiAneufinder_results", "Karyogram.png"),
-        results_gc_corrected= os.path.join(OUT_DIR, SAMPLE, "epi_results", "epiAneufinder_results", "results_gc_corrected.rds"),
-        results_table       = os.path.join(OUT_DIR, SAMPLE, "epi_results", "epiAneufinder_results", "results_table.tsv")
+        cnv_calls            = os.path.join(OUT_DIR, SAMPLE, "epi_results", "epiAneufinder_results", "cnv_calls.rds"),
+        counts_gc_corrected  = os.path.join(OUT_DIR, SAMPLE, "epi_results", "epiAneufinder_results", "counts_gc_corrected.rds"),
+        count_summary        = os.path.join(OUT_DIR, SAMPLE, "epi_results", "epiAneufinder_results", "count_summary.rds"),
+        karyogram            = os.path.join(OUT_DIR, SAMPLE, "epi_results", "epiAneufinder_results", "Karyogram.png"),
+        results_gc_corrected = os.path.join(OUT_DIR, SAMPLE, "epi_results", "epiAneufinder_results", "results_gc_corrected.rds"),
+        results_table        = os.path.join(OUT_DIR, SAMPLE, "epi_results", "epiAneufinder_results", "results_table.tsv")
     threads: 16
+    conda:
+        "../../envs/epi.yml"
     log:
         os.path.join(OUT_DIR, SAMPLE, "logs", "run_epi.log")
     shell:
@@ -54,23 +57,24 @@ rule run_epi:
           2>&1 | tee {log}
         """
 
-rule get_epi_clones
 
-    """this rule will run epianeufinders clone calling function. The output will be a
-    directory, outs/epianeufinder_clones, containing all the results from the clone calling
-    analysis. Furthermore, it will create a tsv file containing a list of cell barcodes that 
-    we will be using for downstream analysis."""
-
+rule get_epi_clones:
+    """Run epiAneufinder clone calling and export cancerous cell barcodes."""
     input:
-        results_table=os.path.join(OUT_DIR, SAMPLE, "epi_results", "epiAneufinder_results", "results_table.tsv"),
-        r_script=os.path.join(BASE_DIR, "workflows", "scripts", "get_epi_clones.R")
+        results_table=os.path.join(
+            OUT_DIR, SAMPLE, "epi_results", "epiAneufinder_results", "results_table.tsv"
+        ),
+        r_script="../scripts/get_epi_clones.R"
     params:
         out_dir=os.path.join(OUT_DIR, SAMPLE, "epi_results", "epiAneufinder_results")
     output:
         cancerous_cells=os.path.join(
-            OUT_DIR, SAMPLE, "epi_results", "epiAneufinder_results", f"{SAMPLE}_cells_for_mquad.tsv"
+            OUT_DIR, SAMPLE, "epi_results", "epiAneufinder_results",
+            f"{SAMPLE}_cells_for_mquad.tsv"
         )
     threads: 16
+    conda:
+        "../../envs/epi.yml"
     log:
         os.path.join(OUT_DIR, SAMPLE, "logs", "get_epi_clones.log")
     shell:
